2e1194130142c293b744dde66fdb0c88a5503e94
Exequielm88 <kekelom88@gmail.com>
2024-03-02 18:07:14 -0300

    agregarSalir

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -10,6 +10,7 @@ const NavBar = () => {
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
+        <a href="#salir">Salir</a>
       </div>
     </div>
   );
